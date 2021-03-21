682231faaa16959f51cbeb186493252ffc3c6127
shruti anand <vitikaawesome81@gmail.com>
2021-03-21 14:14:26 +0530

    added navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {data} from '../data';
+import Navbar from './Navbar';
+
+
 //import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Project Section
+      <Navbar/>
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">
+            Movies
+          </div>
+          <div className="tab">
+            Favourites
+          </div>
+        </div>
+        <div className="list">
+          {data.map(movie=>(
+            <MovieCard movie={movie}/>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+//This component will contain search box
+import React from 'react';
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <div className="nav">
+                <div className="search-container">
+                    <input />
+                    <button className="search-btn">Search</button>
+                </div>
+            </div>
+        );
+    }
+}
