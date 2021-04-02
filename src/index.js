f1e42d1536e098c7b184d577bc4daa037a3a0c6c
shruti anand <vitikaawesome81@gmail.com>
2021-04-02 14:23:13 +0530

    added middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore , applyMiddleware} from 'redux';
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers'
 
+// const logger = function({dispatch,getState}){
+//   return function(next){ //calling middleware
+//     return function(action){
+//       //middleware code
+//       console.log('ACTION_TYPE=',action.type);
+//       next(action); // calling dispatch() func to send undated action
+//     }
+//   }
+// }
+// another way of writing middleware
+const logger= ({dispatch,getState})=>(next)=>(action)=>{
+    console.log('ACTION_TYPE=',action.type);
+      next(action); // calling dispatch() func to send undated action
+}
 
-
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,applyMiddleware(logger));
 console.log(store);
 // console.log('BeforeState',store.getState());
 
